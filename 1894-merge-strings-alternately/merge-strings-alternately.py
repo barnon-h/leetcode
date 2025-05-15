33a08f757e4c62c1bb4e5b41ecbaa3f8ffec6d7b
barnon-h <64619604+barnon-h@users.noreply.github.com>
2025-05-15 12:47:13 -0500

    Time: 41 ms (37.21%) | Memory: 17.8 MB (49.51%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -1,10 +1,10 @@
 class Solution:
-    def mergeAlternately(self, word1: str, word2: str) -> str:
+    def mergeAlternately( self, word1: str, word2: str ) -> str:
         ret = ""
-        final_len = len(word1) if len(word1) > len(word2) else len(word2)
+        final_len = len( word1 ) if len( word1 ) > len( word2 ) else len( word2 )
         for i in range( final_len ):
-            if i < len( word1 ): ret += word1[i]
-            if i < len( word2 ): ret += word2[i]
+            if i < len( word1 ): ret += word1[ i ]
+            if i < len( word2 ): ret += word2[ i ]
         return ret
 
         
