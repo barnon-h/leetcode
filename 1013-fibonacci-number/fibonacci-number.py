cc7393b3301ef462ff50fdb38906a839069cff70
barnon-h <64619604+barnon-h@users.noreply.github.com>
2025-07-05 14:36:17 -0500

    Time: 34 ms (84.96%) | Memory: 17.5 MB (89.40%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -2,7 +2,8 @@ class Solution:
     d = {}
 
     def fib( self, n: int ) -> int:
-        if n == 0 or n == 1: return n
+        if n == 0: return n
+        if n == 1: return n
         if n in self.d.keys(): return self.d[ n ]
         
         self.d[ n ] = self.fib( n - 1 ) + self.fib( n - 2 )
