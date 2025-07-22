c8da13e52c28a8fea254adb2bec36e9b0615dd1f
barnon-h <64619604+barnon-h@users.noreply.github.com>
2025-07-22 17:46:24 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (12.92%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -13,8 +13,9 @@ class Solution:
         elif len( nums ) - idx == 1:
             return nums[ idx ]
         
-        elif len( nums ) - idx == 2:
-            return max([ nums[ idx ], nums[ idx + 1 ]])
+        
+        #lif len( nums ) - idx == 2:
+        #    return max([ nums[ idx ], nums[ idx + 1 ]])
 
         else:
             if idx not in self.d:
