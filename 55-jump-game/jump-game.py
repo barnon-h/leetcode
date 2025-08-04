1d1acbf462d82e1cb5874ccbee53c3d814d3d63e
barnon-h <64619604+barnon-h@users.noreply.github.com>
2025-08-04 14:44:43 -0500

    Time: 8197 ms (5.00%) | Memory: 20.6 MB (5.04%) - LeetSync

---
+++ 55-jump-game/jump-game.py
@@ -0,0 +1,28 @@
+class Solution:
+    d = {}
+    def canJump(self, nums: List[ int ]) -> bool:
+        self.d = {}
+        return self.helper( nums )
+        
+
+    def helper( self, n: List[ int ], idx : int = 0 ) -> bool:
+        if idx == len( n ) - 1:
+            return True
+        
+        elif idx >= len( n ):
+            return False
+        
+        elif n[ idx ] <= 0:
+            return False
+        
+        else:
+            ret = False 
+            
+            for i in range( 1, n[ idx ] + 1 ):
+                if idx + i not in self.d:
+                    self.d[ idx+i ] = self.helper( n, idx + i )
+                
+                if self.d[idx+i] is True: 
+                    ret = True
+                    break
+            return ret
